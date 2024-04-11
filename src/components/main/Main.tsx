bff029260de3f994f4a5e65a03e4d05642f1748b
Ezequiel <zapata.ed1989@gmail.com>
2024-04-11 09:44:26 -0300

    edit text

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -5,7 +5,7 @@ export const Main = () => {
         <div className="p-4 relative  md:flex items-center mt-5 lg:pt-20">
 
             <div className="md:w-1/2 -mt-5 md:flex md:justify-center">
-                <p className="text-balance tracking-wide lg:text-xl mb-2 lg:p-5">Acorta y personaliza tus URLs para una mejor estética y accesibilidad. Impulsa la interacción y optimiza tu estrategia digital con enlaces memorables y datos de rendimiento.</p>
+                <p className="text-balance tracking-wide lg:text-xl mb-2 lg:p-5">Acorta y personaliza tus URLs para una mejor estética y accesibilidad. Impulsa la interacción y optimiza tu estrategia digital con enlaces memorables.</p>
 
             </div>
             <div className="md:w-1/2  md:flex md:justify-center lg:block">
